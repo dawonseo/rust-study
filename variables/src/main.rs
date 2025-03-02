2d97b4bc9d2f0c7b19f6a8b75f6ffff83306fadc
dawonseo <oloielo@naver.com>
2025-03-02 19:05:44 +0900

    수치 연산

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,17 @@
 fn main() {
-    let x = 5;
+    // 덧셈
+    let sum = 5 + 10;
 
-    let x = x + 1;
+    // 뺄셈
+    let difference = 95.5 - 4.3;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
+    // 곱셈
+    let product = 4 * 30;
 
-    println!("The value of x is: {x}");
+    // 나눗셈
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // 결괏값은 -1입니다.
+
+    // 나머지 연산
+    let remainder = 43 % 5;
 }
