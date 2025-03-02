15f9e973b6561519fd3a12bbe10df975b1bd24e5
dawonseo <oloielo@naver.com>
2025-03-02 19:06:53 +0900

    불리언 타입

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,17 +1,4 @@
 fn main() {
-    // 덧셈
-    let sum = 5 + 10;
-
-    // 뺄셈
-    let difference = 95.5 - 4.3;
-
-    // 곱셈
-    let product = 4 * 30;
-
-    // 나눗셈
-    let quotient = 56.7 / 32.2;
-    let truncated = -5 / 3; // 결괏값은 -1입니다.
-
-    // 나머지 연산
-    let remainder = 43 % 5;
+    let t = true;
+    let f: bool = false; // 명시적인 타입 애너테이션
 }
